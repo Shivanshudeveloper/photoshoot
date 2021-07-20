e106aade03b6f9bb65cb87f3bb6f5bb6c811903f
yash-nait <yashnait@gmail.com>
2021-07-20 14:31:58 +0530

    changes

--- dashboard/src/pages/landing/components/Header.js
+++ dashboard/src/pages/landing/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
-import { Link } from 'react-router-dom';
+import { a } from 'react-router-dom';
 
 import {
     isBrowser
@@ -20,9 +20,9 @@ const Header = () => {
                     <div className="row">
                         <div className="col-lg-12">
                             <nav className="navbar navbar-expand-lg">
-                                <Link className="navbar-brand" to="/">
+                                <a className="navbar-brand" href="/">
                                     <img src="https://res.cloudinary.com/dx9dnqzaj/image/upload/v1620378009/photoshooted/Photoshooted_logo_white_bg96p5.png" alt="Logo" />
-                                </Link>
+                                </a>
                                 <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" aria-expanded="false" aria-label="Toggle navigation">
                                     <span style={{backgroundColor: "white"}} className="toggler-icon"></span>
                                     <span style={{backgroundColor: "white"}} className="toggler-icon"></span>
@@ -32,29 +32,29 @@ const Header = () => {
                                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                     <ul id="nav" className="navbar-nav ml-auto">
                                         {/* <li className="nav-item active">
-                                            <Link className="page-scroll" to="/" style={{textDecoration: "none"}}>
+                                            <a className="page-scroll" href="/" style={{textDecoration: "none"}}>
                                                 Home
-                                            </Link>
+                                            </a>
                                         </li>
                                         <li className="nav-item active">
-                                            <Link className="page-scroll" to="/funktionen" style={{textDecoration: "none"}}>
+                                            <a className="page-scroll" href="/funktionen" style={{textDecoration: "none"}}>
                                                 Funktionen
-                                            </Link>
+                                            </a>
                                         </li>
                                         <li className="nav-item active">
-                                            <Link className="page-scroll" to="/preise" style={{textDecoration: "none"}}>
+                                            <a className="page-scroll" href="/preise" style={{textDecoration: "none"}}>
                                                 Preise
-                                            </Link>
+                                            </a>
                                         </li>
                                         <li className="nav-item active">
-                                            <Link className="page-scroll" to="/kontakt" style={{textDecoration: "none"}}>
+                                            <a className="page-scroll" href="/kontakt" style={{textDecoration: "none"}}>
                                                 Kontakt
-                                            </Link>
+                                            </a>
                                         </li>
                                         <li className="nav-item">
-                                            <Link className="page-scroll link" to="/auth/login-unprotected" style={{textDecoration: "none"}}>
+                                            <a className="page-scroll link" href="/auth/login-unprotected" style={{textDecoration: "none"}}>
                                                 Login
-                                            </Link>
+                                            </a>
                                         </li> */}
                                         <li className="nav-item">
                                         <Button style={{ borderRadius: '25px', backgroundColor: 'white', color: 'rgb(104, 82, 237)', fontSize: '11px'}} variant="contained">jetzt starten</Button>
@@ -81,51 +81,51 @@ const Header = () => {
                                     <div className="offcanvas-body offcanvas-text" style={{fontSize: "18px"}}>
                                         <div>
                                             <div style={{margin: "20px"}}>
-                                                <Link to="/" style={{textDecoration: "none", color: "white"}}>
+                                                <a href="/" style={{textDecoration: "none", color: "white"}}>
                                                     Home
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link to="/funktionen" style={{textDecoration: "none", color: "white"}}>
+                                                <a href="/funktionen" style={{textDecoration: "none", color: "white"}}>
                                                     Funktionen
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link to="/preise" style={{textDecoration: "none", color: "white"}}>
+                                                <a href="/preise" style={{textDecoration: "none", color: "white"}}>
                                                     Preise
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link  to="/auth/login-unprotected" style={{textDecoration: "none", color: "white"}}>
+                                                <a  href="/auth/login-unprotected" style={{textDecoration: "none", color: "white"}}>
                                                     Login
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link  to="/kontakt" style={{textDecoration: "none", color: "white"}}>
+                                                <a  href="/kontakt" style={{textDecoration: "none", color: "white"}}>
                                                     Kontact
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link  to="/agb" style={{textDecoration: "none", color: "white"}}>
+                                                <a  href="/agb" style={{textDecoration: "none", color: "white"}}>
                                                     AGB
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link  to="/cookie-richtlinie-eu" style={{textDecoration: "none", color: "white"}}>
+                                                <a  href="/cookie-richtlinie-eu" style={{textDecoration: "none", color: "white"}}>
                                                     Cookie-Richtlinie
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link  to="/cookie-richtlinie-eu" style={{textDecoration: "none", color: "white"}}>
+                                                <a  href="/cookie-richtlinie-eu" style={{textDecoration: "none", color: "white"}}>
                                                     Impressum
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                         </div> 
@@ -142,9 +142,9 @@ const Header = () => {
                     <div className="row">
                         <div className="col-lg-12">
                             <nav className="navbar navbar-expand-lg">
-                                <Link className="navbar-brand" to="/">
+                                <a className="navbar-brand" href="/">
                                     <img src="https://res.cloudinary.com/dx9dnqzaj/image/upload/v1620378077/photoshooted/Photoshooted_A_u2hjii.png" alt="Logo" />
-                                </Link>
+                                </a>
 
                                 <span>
                                 <p style={{color:"#646464", float:"Left",paddingTop: "3px"}}>Menu</p>
@@ -158,30 +158,30 @@ const Header = () => {
                                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                     <ul id="nav" className="navbar-nav ml-auto">
                                         <li className="nav-item active">
-                                            <Link className="page-scroll" to="/" style={{textDecoration: "none"}}>
+                                            <a className="page-scroll" href="/" style={{textDecoration: "none"}}>
                                                 Home
-                                            </Link>
+                                            </a>
                                         </li>
                                         
                                         <li className="nav-item active">
-                                            <Link className="page-scroll" to="/funktionen" style={{textDecoration: "none"}}>
+                                            <a className="page-scroll" href="/funktionen" style={{textDecoration: "none"}}>
                                                 Funktionen
-                                            </Link>
+                                            </a>
                                         </li>
                                         <li className="nav-item active">
-                                            <Link className="page-scroll" to="/preise" style={{textDecoration: "none"}}>
+                                            <a className="page-scroll" href="/preise" style={{textDecoration: "none"}}>
                                                 Preise
-                                            </Link>
+                                            </a>
                                         </li>
                                         <li className="nav-item active">
-                                            <Link className="page-scroll" to="/kontakt" style={{textDecoration: "none"}}>
+                                            <a className="page-scroll" href="/kontakt" style={{textDecoration: "none"}}>
                                                 Kontakt
-                                            </Link>
+                                            </a>
                                         </li>
                                         <li className="nav-item">
-                                            <Link className="page-scroll link" to="/auth/login-unprotected" style={{textDecoration: "none"}}>
+                                            <a className="page-scroll link" href="/auth/login-unprotected" style={{textDecoration: "none"}}>
                                                 Login
-                                            </Link>
+                                            </a>
                                         </li>
                                         <li className="nav-item">
                                             <FacebookIcon style={{ color: '#ffffff', cursor: 'pointer', textDecoration: "none" }} />
@@ -198,33 +198,33 @@ const Header = () => {
                                     <div className="offcanvas-body offcanvas-text">
                                         <div>
                                             <div style={{margin: "20px"}}>
-                                                <Link className="h1" to="/" style={{textDecoration: "none"}}>
+                                                <a className="h1" href="/" style={{textDecoration: "none"}}>
                                                     Home
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link className="h1" to="/funktionen" style={{textDecoration: "none"}}>
+                                                <a className="h1" href="/funktionen" style={{textDecoration: "none"}}>
                                                     Funktionen
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link className="h1" to="/preise" style={{textDecoration: "none"}}>
+                                                <a className="h1" href="/preise" style={{textDecoration: "none"}}>
                                                     Preise
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link className="h1" to="/kontakt" style={{textDecoration: "none"}}>
+                                                <a className="h1" href="/kontakt" style={{textDecoration: "none"}}>
                                                     Kontakt
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                             <div style={{margin: "20px"}}>
-                                                <Link className="h1" to="/auth/login-unprotected" style={{textDecoration: "none"}}>
+                                                <a className="h1" href="/auth/login-unprotected" style={{textDecoration: "none"}}>
                                                     Login
-                                                </Link>
+                                                </a>
                                                 <br></br>
                                             </div>
                                         </div> 
